24429303465e41263adeca21979ffde3679178e6
Ken Kyou <ken.kyou@outlook.com>
2022-09-09 17:24:26 +0800

    [mod] product-list.html.scss

--- js/product-list.js
+++ js/product-list.js
@@ -7,3 +7,13 @@ $(".filter-header > .back").click(function () {
   $(".filter-page").removeClass("filter-show");
   $(".header, .content, .footer").removeClass("list-hidden");
 });
+
+$("ul.menu > a.menu").click(function () {
+  $(".menu-page").addClass("menu-show");
+  //   $(".header, .content, .footer").addClass("list-hidden");
+});
+
+$(".close").click(function () {
+  $(".menu-page").removeClass("menu-show");
+  //   $(".header, .content, .footer").removeClass("list-hidden");
+});
