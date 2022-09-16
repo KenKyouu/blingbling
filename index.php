13c71786f1147767dd5c3068268dc9e026e842ce
Priscilla <priscilla0926152@gmail.com>
2022-09-16 19:05:34 +0800

    [mod] index

--- index.php
+++ index.php
@@ -7,9 +7,26 @@ $pageName = 'home'; // 頁面名稱，可以自定義
 <link rel="stylesheet" href="./styles/index.css" />
 <script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
 <?php include __DIR__ . '/parts/navbar.php'; ?>
+
 <div class="indexContainer">
       <div class="indexBox">
         <div id="area1" class="I-mainSection">
+            <div class="w-indexHeader">
+                <div class="w-indexHeaderBox">
+                    <div class="w-indexHeader-item1">
+                        <img src="./images/icons/index-icon-searcg.svg" alt="">
+                    </div>
+                    <div class="w-indexHeader-item1">
+                        <img src="./images/icons/index-icon-shop.svg" alt="">
+                    </div>
+                    <div class="w-indexHeader-item1">
+                        <img src="./images/icons/index-icon-member.svg" alt="">
+                    </div>
+                    <div class="w-indexHeader-menu">
+                        <img src="./images/icons/index-icon-menu.svg" alt="">
+                    </div>
+                </div>
+            </div>
           <div class="mainTop">
             <div class="mainLogo">
               <img src="./images/logo-1.svg" alt="" />
@@ -49,11 +66,11 @@ $pageName = 'home'; // 頁面名稱，可以自定義
               <h1 slot="header">Canyons</h1>
               <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
             </card>
-            <card data-image="https://images.unsplash.com/photo-1479659929431-4342107adfc1?dpr=2&auto=compress,format&fit=crop&w=1199&h=799&q=80&cs=tinysrgb&crop=">
+            <card data-image="./images/story_card_02.png">
               <h1 slot="header">Beaches</h1>
               <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
             </card>
-            <card data-image="https://images.unsplash.com/photo-1479644025832-60dabb8be2a1?dpr=2&auto=compress,format&fit=crop&w=1199&h=799&q=80&cs=tinysrgb&crop=">
+            <card data-image="./images/story_card_03.png">
               <h1 slot="header">Trees</h1>
               <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
             </card>
@@ -248,9 +265,6 @@ $pageName = 'home'; // 頁面名稱，可以自定義
         </div>
         <div id="area4" class="I-FestivalSection">
           <div class="FestivalBox">
-            <!-- <div class="Festival-TextL">
-              <img src="./images/festival-text.svg" alt="">
-            </div> -->
             <div class="FestivalMainBox">
               <div class="FestivalRow">
                 <div class="FestivalCardRow">
@@ -460,8 +474,6 @@ $pageName = 'home'; // 頁面名稱，可以自定義
               <img src="./images/recommend-text.svg" alt="">
             </div> -->
           </div>
-          
-  
         </div>
         <div id="area5" class="I-newSection">
           <div class="newSection-Top d-flex">
@@ -690,106 +702,172 @@ $pageName = 'home'; // 頁面名稱，可以自定義
           
             </div>
           </div>
-          <div class="NEWSmore">
-            <div class="NEWSbtn d-flex">
-              <div class="NEWSbtn-L">
-                <p>查看更多</p>
-              </div>
-              <div class="NEWSbtn-R">
-                <img src="./images/more-arrow-01.svg" alt="">
-              </div>
+          <a href="./kol.php">
+            <div class="NEWSmore">
+                <div class="NEWSbtn d-flex">
+                <div class="NEWSbtn-L">
+                    <p>查看更多</p>
+                </div>
+                <div class="NEWSbtn-R">
+                    <img src="./images/more-arrow-01.svg" alt="">
+                </div>
+                </div>
             </div>
-          </div>
-          </div>
-        </div>
-    <div id="area7" class="I-brandSection">
-      <div class="brandBox">
-        <div class="brandSlide d-flex">
-          <div class="brandTop d-flex">
-            <div class="brandImgWarp ">
-              <img src="./images/brands/brand-logo-01.svg" alt="">
-            </div>
-            <div class="brandImgWarp ">
-              <img src="./images/brands/brand-logo-02.svg" alt="">
-            </div>
-            <div class="brandImgWarp ">
-              <img src="./images/brands/brand-logo-03.svg" alt="">
-            </div>
-            <div class="brandImgWarp ">
-              <img src="./images/brands/brand-logo-04.svg" alt="">
-            </div>
-            <div class="brandImgWarp ">
-              <img src="./images/brands/brand-logo-05.svg" alt="">
-            </div>
- 
-
-          </div>
-          <div class="brandTop2 d-flex">
-            <div class="brandImgWarp ">
-              <img src="./images/brands/brand-logo-01.svg" alt="">
-            </div>
-            <div class="brandImgWarp ">
-              <img src="./images/brands/brand-logo-02.svg" alt="">
-            </div>
-            <div class="brandImgWarp ">
-              <img src="./images/brands/brand-logo-03.svg" alt="">
-            </div>
-            <div class="brandImgWarp ">
-              <img src="./images/brands/brand-logo-04.svg" alt="">
-            </div>
-            <div class="brandImgWarp ">
-              <img src="./images/brands/brand-logo-05.svg" alt="">
-            </div>
- 
-
+          </a>
           </div>
         </div>
-        <div class="brandSlide d-flex">
-          <div class="brandTop d-flex">
-            <div class="brandImgWarp ">
-              <img src="./images/brands/brand-logo-05.svg" alt="">
-            </div>
-            <div class="brandImgWarp ">
-              <img src="./images/brands/brand-logo-04.svg" alt="">
-            </div>
-            <div class="brandImgWarp ">
-              <img src="./images/brands/brand-logo-03.svg" alt="">
-            </div>
-            <div class="brandImgWarp ">
-              <img src="./images/brands/brand-logo-02.svg" alt="">
-            </div>
-            <div class="brandImgWarp ">
-              <img src="./images/brands/brand-logo-01.svg" alt="">
-            </div>
- 
-
-          </div>
-          <div class="brandTop2 d-flex">
-            <div class="brandImgWarp ">
-              <img src="./images/brands/brand-logo-05.svg" alt="">
-            </div>
-            <div class="brandImgWarp ">
-              <img src="./images/brands/brand-logo-04.svg" alt="">
-            </div>
-            <div class="brandImgWarp ">
-              <img src="./images/brands/brand-logo-03.svg" alt="">
-            </div>
-            <div class="brandImgWarp ">
-              <img src="./images/brands/brand-logo-02.svg" alt="">
-            </div>
-            <div class="brandImgWarp ">
-              <img src="./images/brands/brand-logo-01.svg" alt="">
-            </div>
- 
-
-          </div>
-        </div>
+        <div id="area7" class="I-brandSection">
+            <div class="brandBox">
+                <div class="brandSlide d-flex">
+                <div class="brandTop d-flex">
+                    <div class="brandImgWarp ">
+                    <img src="./images/brands/brand-logo-01.svg" alt="">
+                    </div>
+                    <div class="brandImgWarp ">
+                    <img src="./images/brands/brand-logo-02.svg" alt="">
+                    </div>
+                    <div class="brandImgWarp ">
+                    <img src="./images/brands/brand-logo-03.svg" alt="">
+                    </div>
+                    <div class="brandImgWarp ">
+                    <img src="./images/brands/brand-logo-04.svg" alt="">
+                    </div>
+                    <div class="brandImgWarp ">
+                    <img src="./images/brands/brand-logo-05.svg" alt="">
+                    </div>
+                </div>
+                <div class="brandTop2 d-flex">
+                    <div class="brandImgWarp ">
+                    <img src="./images/brands/brand-logo-01.svg" alt="">
+                    </div>
+                    <div class="brandImgWarp ">
+                    <img src="./images/brands/brand-logo-02.svg" alt="">
+                    </div>
+                    <div class="brandImgWarp ">
+                    <img src="./images/brands/brand-logo-03.svg" alt="">
+                    </div>
+                    <div class="brandImgWarp ">
+                    <img src="./images/brands/brand-logo-04.svg" alt="">
+                    </div>
+                    <div class="brandImgWarp ">
+                    <img src="./images/brands/brand-logo-05.svg" alt="">
+                    </div>
+                </div>
+                </div>
+                <div class="brandSlide d-flex">
+                <div class="brandTop d-flex">
+                    <div class="brandImgWarp ">
+                    <img src="./images/brands/brand-logo-05.svg" alt="">
+                    </div>
+                    <div class="brandImgWarp ">
+                    <img src="./images/brands/brand-logo-04.svg" alt="">
+                    </div>
+                    <div class="brandImgWarp ">
+                    <img src="./images/brands/brand-logo-03.svg" alt="">
+                    </div>
+                    <div class="brandImgWarp ">
+                    <img src="./images/brands/brand-logo-02.svg" alt="">
+                    </div>
+                    <div class="brandImgWarp ">
+                    <img src="./images/brands/brand-logo-01.svg" alt="">
+                    </div>
         
 
-      </div>
+                </div>
+                <div class="brandTop2 d-flex">
+                    <div class="brandImgWarp ">
+                    <img src="./images/brands/brand-logo-05.svg" alt="">
+                    </div>
+                    <div class="brandImgWarp ">
+                    <img src="./images/brands/brand-logo-04.svg" alt="">
+                    </div>
+                    <div class="brandImgWarp ">
+                    <img src="./images/brands/brand-logo-03.svg" alt="">
+                    </div>
+                    <div class="brandImgWarp ">
+                    <img src="./images/brands/brand-logo-02.svg" alt="">
+                    </div>
+                    <div class="brandImgWarp ">
+                    <img src="./images/brands/brand-logo-01.svg" alt="">
+                    </div>
+        
 
+                </div>
+                </div>
+                
+
+            </div>
+        </div>
+    <div class="footer-mobile">
+        <div class="footer-bg">
+            <div class="footer-logo">
+                <img src="./images/logo-footer.svg" alt="" />
+            </div>
+            <div class="footer-nav">
+                <ul>
+                    <li><a href="#">聯絡我們</a></li>
+                    <li><a href="#">幫助中心</a></li>
+                    <li><a href="#">會員中心</a></li>
+                </ul>
+            </div>
+            <div class="footer-social">
+                <a href="#">
+                    <div class="send">
+                        <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M11.1764 0C5.00142 0 0 5.00142 0 11.1764C0 17.3514 5.00142 22.3528 11.1764 22.3528C17.3514 22.3528 22.3528 17.3514 22.3528 11.1764C22.3528 5.00142 17.3514 0 11.1764 0ZM16.5033 7.26605C16.4712 7.71989 16.2145 9.30146 15.9532 11.0114C15.5635 13.4319 15.1464 16.0816 15.1464 16.0816C15.1464 16.0816 15.0822 16.8242 14.5321 16.9526C13.982 17.0809 13.0789 16.4987 12.9184 16.3704C12.7901 16.2741 10.4979 14.8209 9.65444 14.1103C9.42981 13.9178 9.16851 13.5281 9.68653 13.0789C10.8509 12.0153 12.24 10.6905 13.0789 9.84698C13.4685 9.45732 13.8536 8.55422 12.24 9.65444C9.94783 11.236 7.6878 12.7213 7.6878 12.7213C7.6878 12.7213 7.16978 13.0422 6.2025 12.7534C5.23522 12.4646 4.10291 12.0749 4.10291 12.0749C4.10291 12.0749 3.32817 11.589 4.65302 11.0756C4.65302 11.0756 10.2412 8.78343 12.1804 7.9766C12.923 7.6557 15.4444 6.61967 15.4444 6.61967C15.4444 6.61967 16.6088 6.16583 16.5079 7.26605H16.5033Z" fill="white" />
+                        </svg>
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="facebook">
+                        <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M11.8236 0C5.64864 0 0.647217 5.00142 0.647217 11.1764C0.647217 17.3514 5.64864 22.3528 11.8236 22.3528C17.9986 22.3528 23 17.3514 23 11.1764C23 5.00142 17.994 0 11.8236 0ZM16.2199 5.64322H14.9088C13.6207 5.64322 13.2172 6.44546 13.2172 7.26605V9.21435H16.0962L15.6377 12.217H13.2172V19.4693H9.97159V12.217H7.33565V9.21435H9.97159V6.92681C9.97159 4.32754 11.5211 2.88808 13.8911 2.88808C15.028 2.88808 16.2153 3.08979 16.2153 3.08979V5.64322H16.2199Z" fill="white" />
+                        </svg>
+                    </div>
+                </a>
+            </div>
+            <div class="footer-c">
+                <p>&copy;&nbsp;bling&nbsp;bling&nbsp;gift,&nbsp;Inc.</p>
+            </div>
+        </div>
+    </div>
+    <div class="footer-pc">
+        <div class="footer-pc-up">
+            <div class="footer-nav">
+                <ul>
+                    <li><a href="#">聯絡我們</a></li>
+                    <li><a href="#">幫助中心</a></li>
+                    <li><a href="#">會員中心</a></li>
+                </ul>
+            </div>
+            <div class="footer-c">
+                <p>&copy;&nbsp;bling&nbsp;bling&nbsp;gift,&nbsp;Inc.</p>
+            </div>
+            <div class="footer-social">
+                <a href="#">
+                    <div class="send">
+                        <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M11.1764 0C5.00142 0 0 5.00142 0 11.1764C0 17.3514 5.00142 22.3528 11.1764 22.3528C17.3514 22.3528 22.3528 17.3514 22.3528 11.1764C22.3528 5.00142 17.3514 0 11.1764 0ZM16.5033 7.26605C16.4712 7.71989 16.2145 9.30146 15.9532 11.0114C15.5635 13.4319 15.1464 16.0816 15.1464 16.0816C15.1464 16.0816 15.0822 16.8242 14.5321 16.9526C13.982 17.0809 13.0789 16.4987 12.9184 16.3704C12.7901 16.2741 10.4979 14.8209 9.65444 14.1103C9.42981 13.9178 9.16851 13.5281 9.68653 13.0789C10.8509 12.0153 12.24 10.6905 13.0789 9.84698C13.4685 9.45732 13.8536 8.55422 12.24 9.65444C9.94783 11.236 7.6878 12.7213 7.6878 12.7213C7.6878 12.7213 7.16978 13.0422 6.2025 12.7534C5.23522 12.4646 4.10291 12.0749 4.10291 12.0749C4.10291 12.0749 3.32817 11.589 4.65302 11.0756C4.65302 11.0756 10.2412 8.78343 12.1804 7.9766C12.923 7.6557 15.4444 6.61967 15.4444 6.61967C15.4444 6.61967 16.6088 6.16583 16.5079 7.26605H16.5033Z" fill="white" />
+                        </svg>
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="facebook">
+                        <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M11.8236 0C5.64864 0 0.647217 5.00142 0.647217 11.1764C0.647217 17.3514 5.64864 22.3528 11.8236 22.3528C17.9986 22.3528 23 17.3514 23 11.1764C23 5.00142 17.994 0 11.8236 0ZM16.2199 5.64322H14.9088C13.6207 5.64322 13.2172 6.44546 13.2172 7.26605V9.21435H16.0962L15.6377 12.217H13.2172V19.4693H9.97159V12.217H7.33565V9.21435H9.97159V6.92681C9.97159 4.32754 11.5211 2.88808 13.8911 2.88808C15.028 2.88808 16.2153 3.08979 16.2153 3.08979V5.64322H16.2199Z" fill="white" />
+                        </svg>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="footer-logo">
+            <img src="./images/logo-footer.svg" alt="" />
+        </div>
     </div>
    </div> 
+
+
+
 
 <!-- <?php include __DIR__ . '/parts/footer.php'; ?> -->
 <?php include __DIR__ . '/parts/scripts.php'; ?>
