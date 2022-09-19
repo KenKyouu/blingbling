de84fa0805ea17a2d431657ee11110215a38b0b8
yeats <yeatsyeh.0502@gmail.com>
2022-09-19 14:19:23 +0800

    [mod] member-myIntro.php [mod] member-all.scss [mod] member.js

--- js/member.js
+++ js/member.js
@@ -1,1 +1,9 @@
 // greenBorder - memberButton
+
+$("li.myIntro > a").click(function(){
+    if($(this).prop("checked")){
+        $("li.myIntro").css("opacity", "1");
+    } else {
+        $("li.myCollect").css("opacity", "0");
+    }
+});
