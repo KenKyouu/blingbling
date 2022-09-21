f55fad6875b6465d1f364f7a75668a1ba8f9b0a9
Ken Kyou <ken.kyou@outlook.com>
2022-09-21 13:54:16 +0800

    [mod] member-insert

---
+++ js/signup.js
@@ -0,0 +1,44 @@
+const msgc = $("#msgContainer");
+
+function genAlert(msg, type = "danger") {
+  const a = $(`<div class="alert alert-${type}" role="alert">${msg}</div>`);
+  msgc.append(a);
+  setTimeout(() => {
+    a.fadeOut(400, function () {
+      a.remove();
+    });
+  }, 2000);
+}
+
+function checkForm() {
+  // TODO: 檢查欄位資料格式是不是符合
+
+  let isPass = true; // 預設表單的資料是沒問題的
+  // const name = document.signup.name.value;
+  const email = document.signup.email.value;
+
+  // if (name.length < 2) {
+  //     genAlert('請填寫正確的姓名！');
+  //     isPass = false;
+  // }
+  // if (!email) {
+  //     genAlert('請填寫正確的email！');
+  //     isPass = false;
+  // }
+  if (isPass) {
+    // 送出表單資料
+    $.post(
+      "signup-api.php",
+      $(document.signup).serialize(),
+      function (data) {
+        console.log(data);
+        if (data.success) {
+          genAlert("新增完成", "success");
+        } else {
+          genAlert(data.error);
+        }
+      },
+      "json"
+    );
+  }
+}
