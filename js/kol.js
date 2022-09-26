0490348648473aaf21ec05330767995cfb57f130
Stacy <stacy922001@gmail.com>
2022-09-26 17:04:10 +0800

    [mod] kol.all

--- js/kol.js
+++ js/kol.js
@@ -1,16 +1,98 @@
+
+let lastScroll = 0;
+$(window).scroll(function () {
+    let scrollNow = $(window).scrollTop();
+    // console.log('lastScroll:', lastScroll);
+    // console.log('scrollNow', scrollNow);
+
+    if (scrollNow > lastScroll) {
+        $('.header,.up').addClass('header-hidden')
+    }
+    else {
+        $('.header,.up').removeClass('header-hidden')
+    }
+    lastScroll = scrollNow;
+})
+
 let nowPage = 0;
-$('#next').click(function () {
+$('#arrowRightBtn').click(function () {
+    const nowPage = $(this).index();
+    const moveX = nowPage * -800;
+    console.log('nowPage: ', nowPage);
+    console.log('moveX: ', moveX);
+
     if (nowPage < 4) {
         nowPage = nowPage + 1;
     }
-    const moveX = nowPage * -800;
-    $('.kol-slider-train').css('transform', `translateX(${moveX}px)`);
+    $('swiper-train').css('transform', `translateX(${moveX}px)`);
 })
 
-$('#prev').click(function () {
+$('#arrowLeftBtn').click(function () {
     if (nowPage > 0) {
         nowPage = nowPage - 1;
     }
     const moveX = nowPage * -800;
-    $('.kol-slider-train').css('transform', `translateX(${moveX}px)`);
+    $('swiper-train').css('transform', `translateX(${moveX}px)`);
+})
+
+
+// 各文章標籤分類
+$('#mbNewsBeauty,#pcNewsBeauty').click(function () {
+    $('#beautySection').css('display', 'block');
+    $('#defaultAllSection').css('display', 'none');
+    $('#fashionSection').css('display', 'none');
+    $('#momSection').css('display', 'none');
+    $('#houseSection').css('display', 'none');
+    $('#livingSection').css('display', 'none');
+    $('#petSection').css('display', 'none');
+})
+
+$('#mbNewsFashion,#pcNewsFashion').click(function () {
+    $('#beautySection').css('display', 'block');
+    $('#defaultAllSection').css('display', 'none');
+    $('#fashionSection').css('display', 'none');
+    $('#momSection').css('display', 'none');
+    $('#houseSection').css('display', 'none');
+    $('#livingSection').css('display', 'none');
+    $('#petSection').css('display', 'none');
+})
+
+$('#mbNewsMom,#pcNewsMom').click(function () {
+    $('#momSection').css('display', 'block');
+    $('#defaultAllSection').css('display', 'none');
+    $('#fashionSection').css('display', 'none');
+    $('#momSection').css('display', 'none');
+    $('#houseSection').css('display', 'none');
+    $('#livingSection').css('display', 'none');
+    $('#petSection').css('display', 'none');
+})
+
+$('#mbNewsHouse,#pcNewsHouse').click(function () {
+    $('#houseSection').css('display', 'block');
+    $('#defaultAllSection').css('display', 'none');
+    $('#fashionSection').css('display', 'none');
+    $('#momSection').css('display', 'none');
+    $('#houseSection').css('display', 'none');
+    $('#livingSection').css('display', 'none');
+    $('#petSection').css('display', 'none');
+})
+
+$('#mbNewsLiving,#pcNewsLiving').click(function () {
+    $('#livingSection').css('display', 'block');
+    $('#defaultAllSection').css('display', 'none');
+    $('#fashionSection').css('display', 'none');
+    $('#momSection').css('display', 'none');
+    $('#houseSection').css('display', 'none');
+    $('#livingSection').css('display', 'none');
+    $('#petSection').css('display', 'none');
+})
+
+$('#mbNewsPet,#pcNewsPet').click(function () {
+    $('petSection').css('display', 'block');
+    $('#defaultAllSection').css('display', 'none');
+    $('#fashionSection').css('display', 'none');
+    $('#momSection').css('display', 'none');
+    $('#houseSection').css('display', 'none');
+    $('#livingSection').css('display', 'none');
+    $('#petSection').css('display', 'none');
 })
