3ae93ab94d0df50cbe1baca2ab580a8bd82dc4c6
yeats <yeatsyeh.0502@gmail.com>
2022-10-06 17:53:41 +0800

    [mod] member

--- js/member.js
+++ js/member.js
@@ -1,10 +1,8 @@
-// greenBorder - memberButton
+// member-editMyIntro-Lightbox-bs
 
 $('#exampleModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget) // Button that triggered the modal
-    var recipient = button.data('whatever') // Extract info from data-* attributes
-    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var button = $(event.relatedTarget) 
+    var recipient = button.data('whatever') 
     var modal = $(this)
     modal.find('.modal-title').text('New message to ' + recipient)
     modal.find('.modal-body input').val(recipient)
@@ -44,56 +42,3 @@ $('.newcoupon').click(function(){
 $('button.ok').click(function(){
   $('.coupon-lightbox-err').removeClass('show').css('left', '-100%');
 });
-
-// member-button
-
-// $(document).ready(function(){
-//   $('a#btn1').click(function(){
-//     $('a#btn1').addClass('showBtn');
-//     $('a#btn2').removeClass('showBtn');
-//     $('a#btn3').removeClass('showBtn');
-//     $('a#btn4').removeClass('showBtn');
-//     $('a#btn5').removeClass('showBtn');
-//     $('a#btn6').removeClass('showBtn');
-//   });
-//   $('a#btn2').click(function(){
-//     $('a#btn2').addClass('showBtn');
-//     $('a#btn1').removeClass('showBtn');
-//     $('a#btn3').removeClass('showBtn');
-//     $('a#btn4').removeClass('showBtn');
-//     $('a#btn5').removeClass('showBtn');
-//     $('a#btn6').removeClass('showBtn');
-//   });
-//   $('a#btn3').click(function(){
-//     $('a#btn3').addClass('showBtn');
-//     $('a#btn1').removeClass('showBtn');
-//     $('a#btn2').removeClass('showBtn');
-//     $('a#btn4').removeClass('showBtn');
-//     $('a#btn5').removeClass('showBtn');
-//     $('a#btn6').removeClass('showBtn');
-//   });
-//   $('a#btn4').click(function(){
-//     $('a#btn4').addClass('showBtn');
-//     $('a#btn1').removeClass('showBtn');
-//     $('a#btn2').removeClass('showBtn');
-//     $('a#btn3').removeClass('showBtn');
-//     $('a#btn5').removeClass('showBtn');
-//     $('a#btn6').removeClass('showBtn');
-//   });
-//   $('a#btn5').click(function(){
-//     $('a#btn5').addClass('showBtn');
-//     $('a#btn1').removeClass('showBtn');
-//     $('a#btn2').removeClass('showBtn');
-//     $('a#btn3').removeClass('showBtn');
-//     $('a#btn4').removeClass('showBtn');
-//     $('a#btn6').removeClass('showBtn');
-//   });
-//   $('a#btn6').click(function(){
-//     $('a#btn6').addClass('showBtn');
-//     $('a#btn1').removeClass('showBtn');
-//     $('a#btn2').removeClass('showBtn');
-//     $('a#btn3').removeClass('showBtn');
-//     $('a#btn4').removeClass('showBtn');
-//     $('a#btn5').removeClass('showBtn');
-//   });
-// })
