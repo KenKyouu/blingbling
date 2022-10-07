74e92ab48bb804230b62da8682c430d25d377376
Priscilla <priscilla0926152@gmail.com>
2022-10-07 16:43:24 +0800

    [mod] history-api.php、index.php、member-all、package

--- history-api.php
+++ history-api.php
@@ -20,9 +20,17 @@ $sid = isset($_GET['sid']) ? intval($_GET['sid']) : 0;
         //     //檢查資料表是不是有這個商品
             $list = $pdo->query("SELECT * FROM product WHERE sid=$sid")->fetch();
             if(! empty($list)){
+                $newHistory = [];
+                foreach($_SESSION['history'] as $i){
+                    if($i['sid']!=$list['sid']){
+                        $newHistory[] = $i;
+                    }
+                }
 
                 // $_SESSION['history'][$sid] = $list;
-                array_unshift($_SESSION['history'], $list ) ;
+                array_unshift($newHistory, $list ) ;
+
+                $_SESSION['history'] = $newHistory;
             }
         // }
 // }
@@ -34,6 +42,7 @@ if(count($row) > 5){
 
     $_SESSION['history'] = $row;
 }
+
 
 
 echo json_encode($_SESSION['history'], JSON_UNESCAPED_UNICODE);
